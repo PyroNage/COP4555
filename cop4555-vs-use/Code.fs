2af75fb50472dbbe8886275ad822665bc718d431
PyroNage <marcodambrosio96@gmail.com>
2020-01-27 11:04:21 -0500

    Initial commit. Project is set yp how the professor has in his video tutorial.

---
+++ cop4555-vs-use/Code.fs
@@ -0,0 +1,10 @@
+﻿module Code
+
+module Problems1 = 
+
+    let pred n = n - 1
+
+    let test() =
+        printfn "%d" (3 + 4)
+        printfn "pred of 13 is %d" (pred 13)
+        ()
---
+++ cop4555-vs-use/Program.fs
@@ -0,0 +1,10 @@
+﻿[<EntryPoint>]
+let main argv =
+    printfn "Arguments: %A" argv
+
+    Code.Problems1.test()
+
+    printfn "Press enter to continue ..."
+    System.Console.ReadLine() |> ignore
+
+    0 //Return integer exit code
