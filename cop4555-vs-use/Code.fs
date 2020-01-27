1a5f1f114efe4fe877d8c11e44b539d822073f86
PyroNage <marcodambrosio96@gmail.com>
2020-01-27 14:34:29 -0500

    Answered question #2

--- cop4555-vs-use/Code.fs
+++ cop4555-vs-use/Code.fs
@@ -21,8 +21,11 @@ module Problem1 =
 module Problem2 = 
 
     let test() =
-        printfn "%d" (2 + 5 * 10) //Correct
-        //printfn "%A" (10I * 20I) //Error with big int (Not answer though)
-        //printfn "%d" (4 + 5.6) //Incorrect --- This is the answer
-        printfn "%s" ("4" + "5.6") //Correct 
+        printfn "The answer is t1 -> (t2 -> t3), the arrow is right-associative"
+        ()
+
+module Problem3 = 
+
+    let test() =
+        printfn ""
         ()
